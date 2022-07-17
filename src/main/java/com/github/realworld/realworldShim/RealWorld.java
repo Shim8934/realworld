940370f4ce0a9b33350732f0eb7e4dd52481cce1
ShimKiYoung <shim8934@gmail.com>
2022-07-17 15:41:41 +0900

    First Commit Settings

---
+++ src/main/java/com/github/realworld/realworldShim/RealWorld.java
@@ -0,0 +1,13 @@
+package com.github.realworld.realworldShim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorld {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorld.class, args);
+    }
+
+}
